3b20cd68a6a0c59fe936aa2e05b2ba8e60d50225
Henrique Dias <Zephyr.l@hotmail.com>
2021-11-02 15:23:28 -0300

    project 1

---
+++ backend/src/main/java/com/freya/fenrir/Freya/Fenrir/Application.java
@@ -0,0 +1,13 @@
+package com.freya.fenrir.Freya.Fenrir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
